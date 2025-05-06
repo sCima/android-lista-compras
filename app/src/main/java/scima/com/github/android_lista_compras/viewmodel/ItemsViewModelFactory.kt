bd8619fd8b497e523a1700ba8ecc859442082495
sCima <labsfiap@fiap.com.br>
2025-05-05 22:10:01 -0300

    Criacao da ItemsViewModelFactory

--- app/src/main/java/scima/com/github/android_lista_compras/viewmodel/ItemsViewModelFactory.kt
+++ app/src/main/java/scima/com/github/android_lista_compras/viewmodel/ItemsViewModelFactory.kt
@@ -1,4 +1,15 @@
 package scima.com.github.android_lista_compras.viewmodel
 
-class ItemsViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ItemsViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ItemsViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return ItemsViewModel(application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
