79dcca6f4e36a4d820989c9030f16c495b31ed41
sCima <labsfiap@fiap.com.br>
2025-05-05 22:08:41 -0300

    Criacao da ItemsViewModel

--- app/src/main/java/scima/com/github/android_lista_compras/viewmodel/ItemsViewModel.kt
+++ app/src/main/java/scima/com/github/android_lista_compras/viewmodel/ItemsViewModel.kt
@@ -1,4 +1,39 @@
 package scima.com.github.android_lista_compras.viewmodel
 
-class ItemsViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import androidx.room.Room
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import scima.com.github.android_lista_compras.data.ItemDao
+import scima.com.github.android_lista_compras.data.ItemDatabase
+import scima.com.github.android_lista_compras.model.ItemModel
+
+class ItemsViewModel(application: Application) : AndroidViewModel(application) {
+    private val itemDao: ItemDao
+
+    val itemsLiveData: LiveData<List<ItemModel>>
+
+    init {
+        val database = Room.databaseBuilder(
+            getApplication(), ItemDatabase::class.java, "items_database"
+        ).build()
+        itemDao = database.itemDao()
+        itemsLiveData = itemDao.getAll()
+    }
+
+    fun addItem(item: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val newItem = ItemModel(name = item)
+            itemDao.insert(newItem)
+        }
+    }
+
+    fun removeItem(item: ItemModel) {
+        viewModelScope.launch(Dispatchers.IO) {
+            itemDao.delete(item)
+        }
+    }
 }
