17ffaa6f6eadf6d6e39b51c0ac242e60a999c1e4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-12 21:55:23 -0300

    Criação da main

--- app/src/main/java/scima/com/github/android_lista_compras/MainActivity.kt
+++ app/src/main/java/scima/com/github/android_lista_compras/MainActivity.kt
@@ -1,8 +1,12 @@
 package scima.com.github.android_lista_compras
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,13 +14,60 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import scima.com.github.android_lista_compras.ui.theme.AndroidlistacomprasTheme
+import scima.com.github.android_lista_compras.viewmodel.ItemsAdapter
+import scima.com.github.android_lista_compras.viewmodel.ItemsViewModel
+import scima.com.github.android_lista_compras.viewmodel.ItemsViewModelFactory
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: ItemsViewModel;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContentView(R.layout.activity_main)
 
+        // Encontra a barra de ferramentas pelo seu ID e a define como a barra de ação para esta activity.
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        // Define o título da barra de ação.
+        supportActionBar?.title = "Lista de Compras"
+
+        // Encontra o RecyclerView pelo seu ID.
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        // Cria um novo adaptador para o RecyclerView. O adaptador é responsável por exibir os itens na lista.
+        // Quando um item é clicado, ele é removido da lista.
+        val itemsAdapter = ItemsAdapter { item ->
+            viewModel.removeItem(item)
+        }
+        // Define o adaptador do RecyclerView.
+        recyclerView.adapter = itemsAdapter
+
+        // Encontra o botão e o campo de texto pelo seus IDs.
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editText)
+
+        // Define o que acontece quando o botão é clicado.
+        button.setOnClickListener {
+            // Se o campo de texto estiver vazio, exibe um erro e retorna.
+            if (editText.text.isEmpty()) {
+                editText.error = "Preencha um valor"
+                return@setOnClickListener // retorna para o listener
+            }
+
+            // Adiciona o item ao ViewModel e limpa o campo de texto.
+            viewModel.addItem(editText.text.toString())
+            editText.text.clear()
+        }
+
+        // Cria uma nova fábrica para o ViewModel.
+        val viewModelFactory = ItemsViewModelFactory(application)
+        // Obtém uma instância do ViewModel.
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ItemsViewModel::class.java)
+
+        // Observa as mudanças na lista de itens e atualiza o adaptador quando a lista muda.
+        viewModel.itemsLiveData.observe(this) { items ->
+            itemsAdapter.updateItems(items)
         }
     }
 }
